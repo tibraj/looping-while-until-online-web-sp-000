cb0430f5b8d65964a59be1a21462a9a23dfa818a
Taulant Ibraj <tibraj94@gmail.com>
2020-03-27 18:00:02 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
-  levitation_force = 6
-	while levitation_force < 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
+  levitation_force = 6 #the coutner begins at 6
+	while levitation_force < 10 #while levitation_force is less than 10
+    puts "Wingardium Leviosa" #print "Wingardium Leviosa"
+    levitation_force += 1 #increment the counter
   end
 end
